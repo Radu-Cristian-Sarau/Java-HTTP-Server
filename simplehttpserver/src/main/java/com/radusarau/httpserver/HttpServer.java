1678821d0a177cf7d4166365b8de83726ff06f6e
Radu-Cristian-Sarau <radusarau2004@outlook.com>
2024-07-23 15:32:10 +0300

    Added basic html to display on the server

--- simplehttpserver/src/main/java/com/radusarau/httpserver/HttpServer.java
+++ simplehttpserver/src/main/java/com/radusarau/httpserver/HttpServer.java
@@ -3,6 +3,12 @@ package com.radusarau.httpserver;
 import com.radusarau.httpserver.config.Configuration;
 import com.radusarau.httpserver.config.ConfigurationManager;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class HttpServer {
 
     public static void main(String[] args) {
@@ -13,5 +19,33 @@ public class HttpServer {
 
         System.out.println("Server started on port: " + configuration.getPort());
         System.out.println("Webroot: " + configuration.getWebroot());
+
+        try {
+            ServerSocket serverSocket = new ServerSocket(configuration.getPort());
+            Socket socket = serverSocket.accept();
+
+            InputStream inputStream = socket.getInputStream();
+            OutputStream outputStream = socket.getOutputStream();
+
+            String html = "<html><head><title>Simple Java HTTP Server</title></head><body><h1>This page was served using my simple Java HTTP Server</h1></body></html>";
+
+            final String CRLF = "\r\n"; // 13, 10
+
+            String response =
+                    "HTTP/1.1 200 OK" + CRLF + // Status Line: HTTP_VERSION RESPONSE_CODE RESPONSE_MESSAGE
+                    "Content-Length: " + html.getBytes().length + CRLF + // HEADER
+                        CRLF +
+                        html +
+                        CRLF + CRLF;
+
+            outputStream.write(response.getBytes());
+
+            inputStream.close();
+            outputStream.close();
+            socket.close();
+            serverSocket.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
