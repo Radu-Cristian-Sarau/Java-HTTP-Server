30caae4efe043ce1763d32e694337fa5160766aa
Radu-Cristian-Sarau <radusarau2004@outlook.com>
2024-07-23 16:30:21 +0300

    Added worker thread

--- simplehttpserver/src/main/java/com/radusarau/httpserver/core/ServerListenerThread.java
+++ simplehttpserver/src/main/java/com/radusarau/httpserver/core/ServerListenerThread.java
@@ -27,32 +27,23 @@ public class ServerListenerThread extends Thread{
     public void run() {
 
         try {
-            Socket socket = serverSocket.accept();
+            while (serverSocket.isBound() && !serverSocket.isClosed()) {
+                Socket socket = serverSocket.accept();
 
-            LOGGER.info(" * Connection accepted: " + socket.getInetAddress());
+                LOGGER.info(" * Connection accepted: " + socket.getInetAddress());
 
-            InputStream inputStream = socket.getInputStream();
-            OutputStream outputStream = socket.getOutputStream();
+                HttpConnectionWorkerThread workerThread = new HttpConnectionWorkerThread(socket);
+                workerThread.start();
 
-            String html = "<html><head><title>Simple Java HTTP Server</title></head><body><h1>This page was served using my simple Java HTTP Server</h1></body></html>";
-
-            final String CRLF = "\r\n"; // 13, 10
-
-            String response =
-                    "HTTP/1.1 200 OK" + CRLF + // Status Line: HTTP_VERSION RESPONSE_CODE RESPONSE_MESSAGE
-                            "Content-Length: " + html.getBytes().length + CRLF + // HEADER
-                            CRLF +
-                            html +
-                            CRLF + CRLF;
-
-            outputStream.write(response.getBytes());
-
-            inputStream.close();
-            outputStream.close();
-            socket.close();
-            serverSocket.close();
+            }
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            LOGGER.error("Problem with setting socket", e);
+        } finally {
+            if (serverSocket != null) {
+                try {
+                    serverSocket.close();
+                } catch (IOException e) {}
+            }
         }
     }
 }
