d78e663cbcfdacc98b952dfd55c590f1ecece1b4
Radu-Cristian-Sarau <radusarau2004@outlook.com>
2024-07-23 14:50:37 +0300

    Successfully parsed port and webroot

--- simplehttpserver/src/main/java/com/radusarau/httpserver/HttpServer.java
+++ simplehttpserver/src/main/java/com/radusarau/httpserver/HttpServer.java
@@ -1,8 +1,17 @@
 package com.radusarau.httpserver;
 
+import com.radusarau.httpserver.config.Configuration;
+import com.radusarau.httpserver.config.ConfigurationManager;
+
 public class HttpServer {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Server starting...");
+
+        ConfigurationManager.getInstance().loadConfigurationFile("src/main/resources/http.json");
+        Configuration configuration = ConfigurationManager.getInstance().getCurrentConfiguration();
+
+        System.out.println("Server started on port: " + configuration.getPort());
+        System.out.println("Webroot: " + configuration.getWebroot());
     }
 }
