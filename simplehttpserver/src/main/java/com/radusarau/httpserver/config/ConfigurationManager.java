9bf41f52c74a99a5f09dd9474f48950790fd29f7
Radu-Cristian-Sarau <radusarau2004@outlook.com>
2024-07-23 14:06:56 +0300

    Update ConfigurationManager.java
    Defined new methods to load and get configurations.

--- simplehttpserver/src/main/java/com/radusarau/httpserver/config/ConfigurationManager.java
+++ simplehttpserver/src/main/java/com/radusarau/httpserver/config/ConfigurationManager.java
@@ -1,4 +1,33 @@
 package com.radusarau.httpserver.config;
 
 public class ConfigurationManager {
+
+    private static ConfigurationManager myConfigurationManager; // singleton
+    private static Configuration myCurrentConfiguration;
+
+    private ConfigurationManager() {
+
+    }
+
+    public static ConfigurationManager getInstance() {
+        if (myConfigurationManager == null) {
+            myConfigurationManager = new ConfigurationManager();
+        }
+        return myConfigurationManager;
+    }
+
+    /**
+     * Used to load a configuration file by the path provided.
+     * @param filePath - the path to the configuration file
+     */
+    public void loadConfigurationFile(String filePath) {
+
+    }
+
+    /**
+     * Returns the current configuration.
+     */
+    public void getCurrentConfiguration() {
+
+    }
 }
