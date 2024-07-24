b8dae8a2c4e05fb23b8dcf52062cfbef4c326561
Radu-Cristian-Sarau <radusarau2004@outlook.com>
2024-07-24 14:19:58 +0300

    Created HttpMethod enum
    it holds the GET and HEAD

--- simplehttpserver/src/main/java/com/radusarau/httpserver/http/HttpRequest.java
+++ simplehttpserver/src/main/java/com/radusarau/httpserver/http/HttpRequest.java
@@ -2,4 +2,10 @@ package com.radusarau.httpserver.http;
 
 public class HttpRequest extends HttpMessage{
 
+    private String method;
+    private String requestTarget;
+    private String httpVersion;
+
+    HttpRequest() {
+    }
 }
