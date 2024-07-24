01052c376d40449f3c8cacfc19021d4df684ab2a
Radu-Cristian-Sarau <radusarau2004@outlook.com>
2024-07-24 15:30:46 +0300

    Updated Http Method and added test cases

--- simplehttpserver/src/test/java/com/radusarau/httpserver/http/HttpParserTest.java
+++ simplehttpserver/src/test/java/com/radusarau/httpserver/http/HttpParserTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 
 import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 
@@ -21,11 +22,73 @@ class HttpParserTest {
     }
 
     @Test
-    void parseHttpRequest() {
-        httpParser.parseHttpRequest(generateValidTestCase());
+    void parseHttpRequest() throws IOException {
+        HttpRequest request = null;
+        try {
+            request = httpParser.parseHttpRequest(generateValidGETTestCase());
+        } catch (HttpParsingException e) {
+            fail(e);
+        }
+
+        assertEquals(request.getMethod(), HttpMethod.GET);
     }
 
-    private InputStream generateValidTestCase() {
+    @Test
+    void parseHttpRequestBadMethod1() throws IOException {
+        try {
+            HttpRequest request = httpParser.parseHttpRequest(generateBadTestCaseMethodName1());
+            fail();
+        } catch (HttpParsingException e) {
+            assertEquals(e.getErrorCode(), HttpStatusCode.SERVER_ERROR_501_NOT_IMPLEMENTED);
+        }
+
+    }
+
+    @Test
+    void parseHttpRequestBadMethod2() throws IOException {
+        try {
+            HttpRequest request = httpParser.parseHttpRequest(generateBadTestCaseMethodName2());
+            fail();
+        } catch (HttpParsingException e) {
+            assertEquals(e.getErrorCode(), HttpStatusCode.SERVER_ERROR_501_NOT_IMPLEMENTED);
+        }
+
+    }
+
+    @Test
+    void parseHttpRequestInvNumItems1() throws IOException {
+        try {
+            HttpRequest request = httpParser.parseHttpRequest(generateBadTestCaseRequestLineInvNumItems1());
+            fail();
+        } catch (HttpParsingException e) {
+            assertEquals(e.getErrorCode(), HttpStatusCode.CLIENT_ERROR_400_BAD_REQUEST);
+        }
+
+    }
+
+    @Test
+    void parseHttpEmptyRequestLine() throws IOException {
+        try {
+            HttpRequest request = httpParser.parseHttpRequest(generateBadTestCaseEmptyRequestLine());
+            fail();
+        } catch (HttpParsingException e) {
+            assertEquals(e.getErrorCode(), HttpStatusCode.CLIENT_ERROR_400_BAD_REQUEST);
+        }
+
+    }
+
+    @Test
+    void parseHttpRequestLineCRnoLF() throws IOException {
+        try {
+            HttpRequest request = httpParser.parseHttpRequest(generateBadTestCaseRequestLineOnlyCRnoLF());
+            fail();
+        } catch (HttpParsingException e) {
+            assertEquals(e.getErrorCode(), HttpStatusCode.CLIENT_ERROR_400_BAD_REQUEST);
+        }
+
+    }
+
+    private InputStream generateValidGETTestCase() {
         String rawData = "GET / HTTP/1.1\r\n" +
                 "Host: localhost:8080\r\n" +
                 "Connection: keep-alive\r\n" +
@@ -41,6 +104,66 @@ class HttpParserTest {
                 "Sec-Fetch-User: ?1\r\n" +
                 "Sec-Fetch-Dest: document\r\n" +
                 "Accept-Encoding: gzip, deflate, br, zstd\r\n" +
+                "Accept-Language: en-US,en-GB;q=0.9,en;q=0.8,de-DE;q=0.7,de;q=0.6,ro;q=0.5\r\n" +
+                "Cookie: _ga_CEYK81Z395=GS1.1.1721041805.1.0.1721041805.0.0.0; _ga=GA1.1.30712813.1721041806\r\n" +
+                "\r\n";
+
+        InputStream inputStream = new ByteArrayInputStream(rawData.getBytes(StandardCharsets.US_ASCII));
+
+        return inputStream;
+    }
+
+    private InputStream generateBadTestCaseMethodName1() {
+        String rawData = "GeT / HTTP/1.1\r\n" +
+                "Host: localhost:8080\r\n" +
+                "Accept-Language: en-US,en-GB;q=0.9,en;q=0.8,de-DE;q=0.7,de;q=0.6,ro;q=0.5\r\n" +
+                "Cookie: _ga_CEYK81Z395=GS1.1.1721041805.1.0.1721041805.0.0.0; _ga=GA1.1.30712813.1721041806\r\n" +
+                "\r\n";
+
+        InputStream inputStream = new ByteArrayInputStream(rawData.getBytes(StandardCharsets.US_ASCII));
+
+        return inputStream;
+    }
+
+    private InputStream generateBadTestCaseMethodName2() {
+        String rawData = "GETTTT / HTTP/1.1\r\n" +
+                "Host: localhost:8080\r\n" +
+                "Accept-Language: en-US,en-GB;q=0.9,en;q=0.8,de-DE;q=0.7,de;q=0.6,ro;q=0.5\r\n" +
+                "Cookie: _ga_CEYK81Z395=GS1.1.1721041805.1.0.1721041805.0.0.0; _ga=GA1.1.30712813.1721041806\r\n" +
+                "\r\n";
+
+        InputStream inputStream = new ByteArrayInputStream(rawData.getBytes(StandardCharsets.US_ASCII));
+
+        return inputStream;
+    }
+
+    private InputStream generateBadTestCaseRequestLineInvNumItems1() {
+        String rawData = "GET / AAAAAA HTTP/1.1\r\n" +
+                "Host: localhost:8080\r\n" +
+                "Accept-Language: en-US,en-GB;q=0.9,en;q=0.8,de-DE;q=0.7,de;q=0.6,ro;q=0.5\r\n" +
+                "Cookie: _ga_CEYK81Z395=GS1.1.1721041805.1.0.1721041805.0.0.0; _ga=GA1.1.30712813.1721041806\r\n" +
+                "\r\n";
+
+        InputStream inputStream = new ByteArrayInputStream(rawData.getBytes(StandardCharsets.US_ASCII));
+
+        return inputStream;
+    }
+
+    private InputStream generateBadTestCaseEmptyRequestLine() {
+        String rawData = "\r\n" +
+                "Host: localhost:8080\r\n" +
+                "Accept-Language: en-US,en-GB;q=0.9,en;q=0.8,de-DE;q=0.7,de;q=0.6,ro;q=0.5\r\n" +
+                "Cookie: _ga_CEYK81Z395=GS1.1.1721041805.1.0.1721041805.0.0.0; _ga=GA1.1.30712813.1721041806\r\n" +
+                "\r\n";
+
+        InputStream inputStream = new ByteArrayInputStream(rawData.getBytes(StandardCharsets.US_ASCII));
+
+        return inputStream;
+    }
+
+    private InputStream generateBadTestCaseRequestLineOnlyCRnoLF() {
+        String rawData = "GET / HTTP/1.1\r" + // missing LF
+                "Host: localhost:8080\r\n" +
                 "Accept-Language: en-US,en-GB;q=0.9,en;q=0.8,de-DE;q=0.7,de;q=0.6,ro;q=0.5\r\n" +
                 "Cookie: _ga_CEYK81Z395=GS1.1.1721041805.1.0.1721041805.0.0.0; _ga=GA1.1.30712813.1721041806\r\n" +
                 "\r\n";
