a5f032a08660640fbf10e6a2e00d21415b0a8130
Radu-Cristian-Sarau <radusarau2004@outlook.com>
2024-07-24 14:07:12 +0300

    Created HttpParser and tested it

---
+++ simplehttpserver/src/test/java/com/radusarau/httpserver/http/HttpParserTest.java
@@ -0,0 +1,52 @@
+package com.radusarau.httpserver.http;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class HttpParserTest {
+
+    private HttpParser httpParser;
+
+    @BeforeAll
+    public void beforeClass() {
+        httpParser = new HttpParser();
+    }
+
+    @Test
+    void parseHttpRequest() {
+        httpParser.parseHttpRequest(generateValidTestCase());
+    }
+
+    private InputStream generateValidTestCase() {
+        String rawData = "GET / HTTP/1.1\r\n" +
+                "Host: localhost:8080\r\n" +
+                "Connection: keep-alive\r\n" +
+                "Cache-Control: max-age=0\r\n" +
+                "sec-ch-ua: \"Not/A)Brand\";v=\"8\", \"Chromium\";v=\"126\", \"Google Chrome\";v=\"126\"\r\n" +
+                "sec-ch-ua-mobile: ?0\r\n" +
+                "sec-ch-ua-platform: \"Windows\"\r\n" +
+                "Upgrade-Insecure-Requests: 1\r\n" +
+                "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36\r\n" +
+                "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7\r\n" +
+                "Sec-Fetch-Site: cross-site\r\n" +
+                "Sec-Fetch-Mode: navigate\r\n" +
+                "Sec-Fetch-User: ?1\r\n" +
+                "Sec-Fetch-Dest: document\r\n" +
+                "Accept-Encoding: gzip, deflate, br, zstd\r\n" +
+                "Accept-Language: en-US,en-GB;q=0.9,en;q=0.8,de-DE;q=0.7,de;q=0.6,ro;q=0.5\r\n" +
+                "Cookie: _ga_CEYK81Z395=GS1.1.1721041805.1.0.1721041805.0.0.0; _ga=GA1.1.30712813.1721041806\r\n" +
+                "\r\n";
+
+        InputStream inputStream = new ByteArrayInputStream(rawData.getBytes(StandardCharsets.US_ASCII));
+
+        return inputStream;
+    }
+}
